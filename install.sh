da62c699dc5563da0450e1f6e791006e1e4082f7
Behçet Şentürk <behcet.senturk@protonmail.com>
2019-06-02 20:15:07 +0300

    Install script and .gitignore altered.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-sudo apt install python3-tk
+sudo apt install python3-tk python3-pip
+wget https://pjreddie.com/media/files/yolov3.weights > ./yolo-coco
+pip3 install -r requirements.txt --user
