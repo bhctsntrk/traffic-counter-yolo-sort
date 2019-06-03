f4ba761be339e46265575e9335150f15ddc42802
Behçet Şentürk <behcet.senturk@protonmail.com>
2019-06-03 12:58:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 sudo apt install python3-tk python3-pip
-wget https://pjreddie.com/media/files/yolov3.weights > ./yolo-coco
+wget https://pjreddie.com/media/files/yolov3.weights > ./yolo-coco/yolov3.weights
 pip3 install -r requirements.txt --user
