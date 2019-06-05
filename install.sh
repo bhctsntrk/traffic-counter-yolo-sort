5d7b411f58e097045a4d0d99226b7746e54483b7
Huseyin ERDEM <rootofarch@gmail.com>
2019-06-06 00:46:42 +0300

    install script and gitignore modified to use new weights

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+export fileid=10F4n3uFXMmGfCzZ-U_Etqk7Lk22B3mm2
+export filename=./yolo-coco/yolov3-aerial.weights
+
+wget -O /tmp/gdrivedl 'https://f.mjh.nz/gdrivedl'
+chmod +x /tmp/gdrivedl
+/tmp/gdrivedl ${fileid} ${filename}
+
 sudo apt install python3-tk python3-pip
-wget -O ./yolo-coco/yolov3.weights https://pjreddie.com/media/files/yolov3.weights
 pip3 install -r requirements.txt --user
