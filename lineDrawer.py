20fef1e1f9e955924d47d8ee4179caea029e3c2b
Behçet Şentürk <behcet.senturk@protonmail.com>
2019-06-06 04:42:43 +0300

    Warnings are showed in black area.

--- lineDrawer.py
+++ lineDrawer.py
@@ -2,25 +2,43 @@ import cv2
 import copy
 
 class LineDrawer():
-    def __init__(self, frame):
+    def __init__(self, frame, black_border_height):
         self.drawing = False
         self.lines = []
         self.p1 = None
         self.p2 = None
         self.frame = frame
 
+        self.title = "Draw Lines"
+
+        self.black_border_height = black_border_height
+
     def on_mouse(self, event, x, y, flags, params):
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.p1 = (x,y)
+            self.p1 = (x,y-self.black_border_height)  # Because black area is in upper section of image and 
 
         elif event == cv2.EVENT_LBUTTONUP:
-            self.p2 = (x,y)
+            self.p2 = (x,y-self.black_border_height)  # we need to substract height from y values of lines 
             self.lines.append([self.p1, self.p2])
 
 
         elif event == cv2.EVENT_MOUSEMOVE:
             if self.drawing is True:
                 self.p2 = (x,y)
+
+    def show_output(self, frame):
+        # Show image with warnings
+        
+        b_frame = cv2.copyMakeBorder(self.frame,self.black_border_height,1,1,1,cv2.BORDER_CONSTANT,value=[0,0,0])
+
+        text = ["Please draw at least two lines in frame",
+                "Press r key to reset.",
+                "Press o key when you finish it."]
+
+        for indx,i in enumerate(text):
+            cv2.putText(b_frame, i, (40, 30*(indx+1)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, [255,255,255], 2)
+
+        cv2.imshow(self.title, b_frame)
 
     def drawer(self):
         print("Please draw lines in frame")
@@ -30,19 +48,20 @@ class LineDrawer():
         # We need deep copy to copy original frame
         frame_pure_copy = copy.deepcopy(self.frame)
 
-        cv2.namedWindow('Draw lines (press r to reset) (press o to complete)')
-        cv2.setMouseCallback('Draw lines (press r to reset) (press o to complete)', self.on_mouse)
+        cv2.namedWindow(self.title)
+        cv2.setMouseCallback(self.title, self.on_mouse)
 
         while True:
             
             if self.lines:
                 for indx, l in enumerate(self.lines):
-                    if indx % 2 == 0:
                         cv2.line(self.frame, l[0], l[1], (0,255,0), 5)
-                    else:
-                        cv2.line(self.frame, l[0], l[1], (0,0,255), 5)
 
-            cv2.imshow('Draw lines (press r to reset) (press o to complete)', self.frame)
+            self.show_output(self.frame)
+
+            # We need to do this because event handler too fast and too many border added to image
+            # So we reset image every iteration
+            self.frame = frame_pure_copy
             
             key = cv2.waitKey(1)
             if key == ord('o'):
